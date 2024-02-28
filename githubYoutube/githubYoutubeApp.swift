1da5b112f942906140c7a07c4671034ea1ee3cd1
Bruno Marques <bruno.marques@townsq.com.br>
2024-02-27 21:43:46 -0300

    primeiro commit

---
+++ githubYoutube/githubYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubYoutubeApp.swift
+//  githubYoutube
+//
+//  Created by Bruno Marques on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct githubYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
